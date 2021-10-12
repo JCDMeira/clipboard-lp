9a5b6887a82b5cdb0cea3b59a365c0a4543efdc6
Jean Meira <jean.meira10@hotmail.com>
2021-10-12 10:08:29 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GlobalStyled } from './global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyled />
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyled = createGlobalStyle`
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html{
+  font-size: 62.5%;
+  font-family: "Bai Jamjuree";
+}
+`;
